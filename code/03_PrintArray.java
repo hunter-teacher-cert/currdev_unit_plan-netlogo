8db9c412c3958d7588120b76951ed4de3a34f490
Chris O'Brien <cobrien528@gmail.com>
2021-12-19 21:06:56 -0500

    lesson plans

---
+++ code/03_PrintArray.java
@@ -0,0 +1,14 @@
+public class PrintArray
+{
+    public static void main(String[] args)
+    {
+        String[] arr = new String[]{"w", "x", "y", "z"};
+        printArr(arr);
+    }
+
+    public static void printArr(String[] arr)
+    {
+        // Print everything in the array on its own line
+
+    }
+}
